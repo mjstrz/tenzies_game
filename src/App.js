dbd0a13f592d42ad2dbab904d109834076dade34
mjstrz <strzelecki.monica7@gmail.com>
2022-03-30 23:41:46 -0400

    allNewDice() - generates array of 10 random numbers

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import React from 'react';
 import Dice from './components/dice';
 
 export default function App() {
+
+  function allNewDice() {
+
+    const newDice = [] //new array to hold the numbers
+    for (let i = 0; i < 10; i++) { // loop 10 times
+        newDice.push(Math.ceil(Math.random() * 6)) // push a random number from 1-6 to the array
+      }
+      return newDice // return array 
+  }  
+    console.log(allNewDice());
+  
+
   return (
     <main>
         <div className='dice-container'>
