667d8a8b51c65c212241878726ee7ebde182642a
mjstrz <strzelecki.monica7@gmail.com>
2022-03-31 15:34:01 -0400

    Change dice to objects (instead of array of numbers)

--- src/App.js
+++ src/App.js
@@ -2,24 +2,20 @@ import './App.css';
 import React, { useState } from 'react';
 import Die from './components/die';
 import Button from 'react-bootstrap/Button';
+import {nanoid} from 'nanoid';
 
 export default function App() {
-
-/**
- * Challenge: Create a `Roll Dice` button that will re-roll
- * all 10 dice
- * 
- * Clicking the button should generate a new array of numbers
- * and set the `dice` state to that new array (thus re-rendering
- * the array to the page)
- */
 
   const [dice, setDice] = useState(allNewDice())
 
   function allNewDice() {
     const newDice = [] //new array to hold the numbers
     for (let i = 0; i < 10; i++) { // loop 10 times
-        newDice.push(Math.ceil(Math.random() * 6)) // push a random number from 1-6 to the array
+        newDice.push({
+          value: Math.ceil(Math.random() * 6), // Update the array of numbers in state to be
+          isHeld: false,                      // an array of objects instead.
+          id: nanoid() // nanoid - a fast way to generate a random ID 
+        }) 
       }
       return newDice // return array 
   }  
@@ -30,7 +26,8 @@ export default function App() {
       setDice(allNewDice())
   }
   
-  const diceElements = dice.map(die => <Die value={dice} />)
+  const diceElements = dice.map(die => (
+      <Die key={die.id} value={die.value} />)) //die.id is created by nanoID 
 
   return (
     <main>
