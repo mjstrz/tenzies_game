b38daa1c266661f462a1ce0b99af3dcc69980feb
mjstrz <strzelecki.monica7@gmail.com>
2022-03-30 23:30:40 -0400

    Dice component created; styles added to dice

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
+import React from 'react';
+
+import Dice from './components/dice';
 
 export default function App() {
   return (
-    <main> </main>
+    <main>
+        <div className='dice-container'>
+            <Dice value="1" />
+            <Dice value="1"/>
+            <Dice value="1"/>
+            <Dice value="1"/>
+            <Dice value="1"/>
+            <Dice value="1"/>
+            <Dice value="1"/>
+            <Dice value="1"/>
+            <Dice value="1"/>
+            <Dice value="1"/>
+        </div> 
+    </main>
   );
 }
 
