860d0210cfc84ccc3f67bb549d05257e20c6444d
mjstrz <strzelecki.monica7@gmail.com>
2022-03-31 15:49:18 -0400

    Styling held dice

--- src/App.js
+++ src/App.js
@@ -6,6 +6,15 @@ import {nanoid} from 'nanoid';
 
 export default function App() {
 
+  /**
+ * Challenge: Add conditional styling to the Die component
+ * so that if it's held (isHeld === true), its background color
+ * changes to a light green (#59E391)
+ * 
+ * Remember: currently the Die component has no way of knowing
+ * if it's "held" or not.
+ */
+
   const [dice, setDice] = useState(allNewDice())
 
   function allNewDice() {
@@ -27,7 +36,7 @@ export default function App() {
   }
   
   const diceElements = dice.map(die => (
-      <Die key={die.id} value={die.value} />)) //die.id is created by nanoID 
+      <Die key={die.id} value={die.value} isHeld={die.isHeld} />)) //die.id is created by nanoID 
 
   return (
     <main>
--- src/components/die.js
+++ src/components/die.js
@@ -1,8 +1,12 @@
 import React from 'react'; 
 
 export default function Die(props) {
+
+    const styles = {
+        backgroundColor: props.isHeld ? "#59E391" : "white"
+    }
     return (
-        <div className='dice-face'>
+        <div className='dice-face' style={styles}>
             <h2 className='dice-num'>{props.value}</h2>
         </div>
     )
